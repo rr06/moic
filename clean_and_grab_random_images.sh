e55c16f4fff493d3084647f0845818d59a05d5e5
Rob Romito <rob@robertromito.com>
2020-09-06 14:27:08 -0400

    Clearing out full training_images dir

--- clean_and_grab_random_images.sh
+++ clean_and_grab_random_images.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-rm images/training_images/*.JPG
+rm images/training_images/*
+rm images/training_images/{smooth,wrinkly,inbetween}/*
 
 for i in $(find ./images/raw_data_set/* | sort -R | tail -n $1)
 do
