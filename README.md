d72dfeabe312cedabef0c528aa2f1f297cd9758b
Rob Romito <rob@robertromito.com>
2020-10-25 13:33:51 -0400

    Adjusted classifier to read from training instructions file and take command line args

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # moic
 Micro Organism Image Classifer experiment
+
+## Collaboration
+
+moic provides a system to iteratively classify a set of biofilm images at different stages of wrinkling.
+
+The classification process is a partnership between a bioligst and a software developer. 
+
+#### Biologist
+
+* Get the full set of images to classify
+* Manually classifies a subset of images for model training
+* Reviews classification produced by model
+* Further manual classification of images classified prior
+
+#### Developer
+
+* Comes up with data strucutures and file formats for manual classification
+* Develops way to easily share images, training classifications, and final results
+* Writes code to execute model
+* Executes model to produce classification
+* Builds tools for Biologist to easily review output of model
--- src/moic_classifier.js
+++ src/moic_classifier.js
@@ -1,24 +1,26 @@
-/*
- * Step: Load all the node modules we'll need for the program
- */
 const fs = require('fs')
 const path = require('path')
 const tf = require('@tensorflow/tfjs-node')
 const mobilenet = require('@tensorflow-models/mobilenet')
 const knnClassifier = require('@tensorflow-models/knn-classifier')
 
-/* 
- * Step: Define constants we'll use throughout the program
- */
-const CLASSIFICATIONS = ["smooth", "wrinkly", "inbetween"]
-const DIR_TRAIN_IMAGES = "images/training_images/"
-const DIR_RAW_DATA_SET = "images/raw_data_set"
+const myArgs = process.argv.slice(2)
+
+const DIR_RAW_DATA_SET = myArgs[0]
+console.log(`Image directory: ${DIR_RAW_DATA_SET}`)
+
+console.log(`Training classification file: ${myArgs[1]}`)
+const training_instructions = JSON.parse(fs.readFileSync(myArgs[1]))
+
+const CLASSIFICATIONS = training_instructions.map(i => i["classification"]);
+console.log(`Classifications: ${CLASSIFICATIONS}`)
+
 const PHASE_INIT_TF = "INIT"
 const PHASE_TRAIN = "TRAIN"
 const PHASE_CLASSIFY = "CLASSIFY"
 
 /*
-* Step: Define some helpful functions to elimnate duplicate code 
+* Step: Define some helpful functions to eliminate duplicate code 
 */
 const readImage = path => {
     const imageBuffer = fs.readFileSync(path)
@@ -59,15 +61,15 @@ async function moic() {
          */
         console.time(PHASE_TRAIN)
         console.timeLog(PHASE_TRAIN, `Looking for training images...`)
-        CLASSIFICATIONS.forEach(c => {
+        training_instructions.forEach(ti => {
+            let c = ti["classification"]
             classification_results[c] = {
                 training_images: 0,
                 matches: []
             }
-            const classification_path = path.join(DIR_TRAIN_IMAGES, c)
-            const training_files = fs.readdirSync(classification_path)
-            training_files.forEach((f) => {
-                const imagePath = path.join(classification_path, f)
+            
+            ti["images"].forEach(f => {
+                const imagePath = path.join(DIR_RAW_DATA_SET, f)
                 classifier.addExample(
                     model.infer(readImage(imagePath)),
                     c
@@ -87,7 +89,6 @@ async function moic() {
                 const prediction = await classifier.predictClass(
                     model.infer(readImage(path.join(DIR_RAW_DATA_SET, i)))
                 )
-                console.log(`${i}\t${prediction.label}\t${JSON.stringify(prediction.confidences)}`)
                 classification_results[prediction.label].matches.push({
                     "image": i,
                     "confidences": prediction.confidences
@@ -105,6 +106,8 @@ async function moic() {
         for(const c of CLASSIFICATIONS) {
             printReportFor(c, classification_results[c])
         }
+
+        fs.writeFileSync(myArgs[2], JSON.stringify(classification_results))
 
     } catch (err) {
         console.error(err)
