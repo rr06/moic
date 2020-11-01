97ba3f52039e9469ef0e38aceb308c75cdbd2671
Rob Romito <rob@robertromito.com>
2020-11-01 14:24:51 -0500

    Visual improvements

--- src/Match.svelte
+++ src/Match.svelte
@@ -1,7 +1,16 @@
 <script>
   import lazyLoad from "./lazyLoad";
-
   export let match;
+
+  function format(confidence) {
+    if (confidence == 1 || confidence == 0) return confidence;
+    return confidence.toFixed(3);
+  }
+
+  function background(confidence) {
+    if (confidence == 1) return "has-background-success";
+    return "";
+  }
 </script>
 
 <div class="card">
@@ -15,7 +24,9 @@
   </div>
   <div class="card-footer has-text-centered">
     {#each Object.keys(match.confidences) as c}
-      <p class="card-footer-item">{c}<br />{match.confidences[c].toFixed(3)}</p>
+      <p class="card-footer-item {background(match.confidences[c])}">
+        {c}<br />{format(match.confidences[c])}
+      </p>
     {/each}
   </div>
 </div>
